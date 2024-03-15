f853741ca1832373c3461f5dc7e3ae579169a54a
xafero <xafero@users.noreply.github.com>
2024-03-15 22:45:44 +0100

    Adding bool to test

--- test/MultiParse.Tests/ParserTest.cs
+++ test/MultiParse.Tests/ParserTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace MultiParse.Tests
@@ -8,11 +9,14 @@ namespace MultiParse.Tests
         [InlineData("(3+cos(9.0f/2)*2f)/(3+2)", "0.515681680227688")]
         [InlineData("(3+sin(9.0f/2)*2)/(3+2)", "0.208987952933961")]
         [InlineData("3+sqrt(2)", "4.414213562373095")]
+        [InlineData("true", "True")]
+        [InlineData("false", "False")]
         public void TestSimple(string input, string output)
         {
             var e = new Expression();
             var result = e.Evaluate(input);
-            var text = result.ToString().Replace(',', '.').Substring(0, 17);
+            var text = result.ToString();
+            text = text?.Replace(',', '.').Substring(0, Math.Min(text.Length, 17));
             Assert.Equal(output, text);
         }
     }
