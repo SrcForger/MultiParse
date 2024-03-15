152cd7102e205ff12510c077c2367844e4ecc91e
xafero <xafero@users.noreply.github.com>
2024-03-15 22:28:00 +0100

    Added test

--- test/MultiParse.Tests/ParserTest.cs
+++ test/MultiParse.Tests/ParserTest.cs
@@ -7,6 +7,7 @@ namespace MultiParse.Tests
         [Theory]
         [InlineData("(3+cos(9.0f/2)*2f)/(3+2)", "0.515681680227688")]
         [InlineData("(3+sin(9.0f/2)*2)/(3+2)", "0.208987952933961")]
+        [InlineData("3+sqrt(2)", "4.414213562373095")]
         public void TestSimple(string input, string output)
         {
             var e = new Expression();
